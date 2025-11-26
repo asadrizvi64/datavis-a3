376e3ba43cd24a9952f7a5b9b978441296b75911
Julián Méndez <menoc.sk27@gmail.com>
2025-11-26 15:57:21 +0100

    init

---
+++ main.js
@@ -0,0 +1,11 @@
+var d3; // Minor workaround to avoid error messages in editors
+
+// Waiting until document has loaded
+window.onload = () => {
+
+  // YOUR CODE GOES HERE
+  console.log("YOUR CODE GOES HERE");
+
+  // Load the data set from the assets folder:
+
+};
